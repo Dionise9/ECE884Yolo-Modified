70deed71f791fe94f0930f5eb267fdea3359043a
Nicholas Dionise <dionise9@msu.edu>
2022-04-02 23:28:18 -0400

    Switched yolo base models

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from tf2_yolov4.model import YOLOv4
 
 HEIGHT, WIDTH = (640, 960)
 
+model = YOLOv4(
+    (HEIGHT, WIDTH, 3),
+    80,
+    YOLOV4_ANCHORS,
+    weights="darknet",
+)
+
 image = tf.io.read_file("./Examples/IMG_2878.PNG")
 image = tf.image.decode_image(image)
 image = tf.image.resize(image, (HEIGHT, WIDTH))
--- tf2_yolov4/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-YoloV4 in Tensorflow 2
-"""
-
-__version__ = "0.1.0"
